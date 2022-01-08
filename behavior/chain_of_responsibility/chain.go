e167b6ef1ff194454f35ee56a1a98cd337c2e428
majm <junmin.ma@shopee.com>
2022-01-08 21:34:26 +0800

    [go-pattern][init][1. chain of responsibility]

---
+++ behavior/chain_of_responsibility/chain.go
@@ -0,0 +1,64 @@
+// Created By: junmin.ma
+// Description: <description>
+// Date: 2022-01-08 20:47
+package chain_of_responsibility
+
+import "strings"
+
+// Context  携带消息的 上下文信息
+type Context struct {
+	param string
+}
+
+// Handler provider a handle interface
+type Handler interface {
+	handle(msg *Context) string
+}
+
+type ConcreteHandlerA struct {
+	next Handler
+}
+
+func (c *ConcreteHandlerA) handle(ctx *Context) string {
+	if strings.Contains(ctx.param, "3") {
+		// ConcreteHandlerA doSomething
+		return "c"
+	}
+	if c.next != nil {
+		return c.next.handle(ctx)
+	}
+
+	return ""
+}
+
+type ConcreteHandlerB struct {
+	next Handler
+}
+
+func (c *ConcreteHandlerB) handle(ctx *Context) string {
+	if strings.Contains(ctx.param, "2") {
+		// ConcreteHandlerB doSomething
+		return "b"
+	}
+	if c.next != nil {
+		return c.next.handle(ctx)
+	}
+
+	return ""
+}
+
+type ConcreteHandlerC struct {
+	next Handler
+}
+
+func (c *ConcreteHandlerC) handle(ctx *Context) string {
+	if strings.Contains(ctx.param, "3") {
+		// ConcreteHandlerC doSomething
+		return "c"
+	}
+	if c.next != nil {
+		return c.next.handle(ctx)
+	}
+
+	return ""
+}
